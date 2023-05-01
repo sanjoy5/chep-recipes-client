da6042cd843ea376fd8a42e98be7c57220d6f096
Sanjoy Sarker <sarkerd52@gmail.com>
2023-05-02 04:01:46 +0600

    chefs data added from server side

---
+++ src/components/Chefs.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import { BiLike } from 'react-icons/bi';
+
+const Chefs = ({ chefs }) => {
+    return (
+        <>
+            <section className="text-gray-600 body-font">
+                <div className="w-100 md:w-10/12 px-5 py-24 mx-auto">
+                    <div className="text-center mb-20">
+                        <h1 className="sm:text-3xl text-2xl font-medium title-font text-gray-900 mb-4">Our Chefs</h1>
+                        <div className="flex mt-6 justify-center">
+                            <div className="w-16 h-1 rounded-full bg-red-500 inline-flex"></div>
+                        </div>
+                    </div>
+                    <div className="flex flex-wrap -m-4">
+                        {
+                            chefs?.map(chef => (
+                                // <div key={chef.id} className="p-4 lg:w-1/2">
+                                //     <div className="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
+                                //         <img alt="team" className="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-top sm:mb-0 mb-4" src={chef.chef_picture} />
+                                //         <div className="flex-grow sm:pl-8">
+                                //             <h2 className="title-font font-medium text-lg text-gray-900">{chef.chef_name}</h2>
+                                //             <h3 className="text-gray-500 mb-1">Experience: {chef.years_of_experience} year</h3>
+                                //             <p className="">Recipes: {chef.numbers_of_recipes} </p>
+                                //             <p className="my-2 flex items-center gap-1"><BiLike className='text-xl text-red-500' /> <span className="">{chef.likes}</span></p>
+                                //             <button className="py-2 px-5 rounded bgc-primary">Details</button>
+                                //         </div>
+                                //     </div>
+                                // </div>
+
+                                <div class="p-4 lg:w-1/2">
+                                    <div class="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
+                                        <img alt="team" class="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-top sm:mb-0 mb-4" src={chef.chef_picture} />
+                                        <div class="flex-grow sm:pl-8">
+                                            <h2 class="title-font font-medium text-lg text-gray-900">{chef.chef_name}</h2>
+                                            <h3 class="text-gray-500 mb-2">Experience: {chef.years_of_experience} year</h3>
+                                            <p class="mb-2">{chef.bio}</p>
+                                            <div className='flex items-center justify-center sm:justify-start gap-4 mb-4'>
+                                                <p className="">Recipes: {chef.numbers_of_recipes} </p>
+                                                <p className="flex items-center gap-1"><BiLike className='text-xl text-red-500' /> <span className="">{chef.likes}</span></p>
+                                            </div>
+                                            <span class="inline-flex">
+                                                <button className="py-2 px-5 rounded bgc-primary">Details</button>
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div>
+
+
+                            ))
+                        }
+
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Chefs
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Banner from '../components/Banner'
 import Services from '../components/Services'
+import Chefs from '../components/Chefs'
+import { useLoaderData } from 'react-router-dom'
 
 const Home = () => {
+
+    const chefs = useLoaderData()
+    console.log('Chefs : ', chefs);
     return (
         <>
             <Banner></Banner>
             <Services></Services>
+            <Chefs chefs={chefs} />
         </>
     )
 }
