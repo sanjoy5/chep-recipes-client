a0c035cd769c121dc2c4e3d1791f050ccbb574b5
Sanjoy Sarker <sarkerd52@gmail.com>
2023-05-02 21:52:59 +0600

    Page loading part added

---
+++ src/components/Loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loading = () => {
+    return (
+        <div className='h-96 w-full flex items-center justify-center'>
+            <h2 className="color-primary text-2xl md:text-4xl font-bold">Loading...</h2>
+        </div>
+    )
+}
+
+export default Loading
--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import Header from '../components/Header'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigation } from 'react-router-dom'
 import Footer from '../components/Footer'
 import { Toaster } from 'react-hot-toast';
+import Loading from '../components/Loading';
 
 const Main = () => {
+    const navigation = useNavigation()
     return (
         <>
             <Header></Header>
+
+            <div className="">{navigation.state === 'loading' && <Loading />}</div>
+
             <Outlet></Outlet>
             <Footer />
             <Toaster />
