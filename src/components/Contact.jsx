b93dcdca65022d02b9d6ff83a10a00ae19d66015
Sanjoy Sarker <sarkerd52@gmail.com>
2023-05-02 20:41:51 +0600

    footer and Extra section updated

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Contact = () => {
     return (
         <>
-            <section className="text-gray-600 body-font relative">
+            {/* <section className="text-gray-600 body-font relative">
                 <div className="container px-5 pb-16 mx-auto">
                     <div className="text-center mb-16">
                         <h1 className="sm:text-3xl text-2xl font-medium title-font text-gray-900 mb-4">Contact Us</h1>
@@ -35,6 +35,54 @@ const Contact = () => {
                                 <button className="flex mx-auto text-white bg-red-500 border-0 py-2 px-8 focus:outline-none hover:bg-red-600 rounded text-lg">Message</button>
                             </div>
                         </div>
+                    </div>
+                </div>
+            </section> */}
+
+
+            <section className="text-gray-600 body-font relative">
+                <div className="text-center mb-16">
+                    <h1 className="sm:text-3xl text-2xl font-medium title-font text-gray-900 mb-4">Get In Touch</h1>
+                    <div className="flex mt-6 justify-center">
+                        <div className="w-16 h-1 rounded-full bg-red-500 inline-flex"></div>
+                    </div>
+                </div>
+                <div className="w-full md:w-10/12 px-5 pb-24 mx-auto flex sm:flex-nowrap flex-wrap">
+
+                    <div className="lg:w-2/3 md:w-1/2 bg-gray-300 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
+
+                        <iframe width="100%" height="100%" className="absolute inset-0" frameborder="0" title="map" marginheight="0" marginwidth="0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d448193.951015035!2d76.76357497207219!3d28.644287357064808!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390cfd5b347eb62d%3A0x37205b715389640!2sDelhi%2C%20India!5e0!3m2!1sen!2sbd!4v1683036191362!5m2!1sen!2sbd" style={{ filter: 'grayscale(0.2)  opacity(0.9)' }} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+                        <div className="bg-white relative flex flex-wrap py-6 rounded shadow-md">
+                            <div className="lg:w-1/2 px-6">
+                                <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs">ADDRESS</h2>
+                                <p className="mt-1">C 5/42, Safdarjung Devp Area, Delhi, 110016, India</p>
+                            </div>
+                            <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
+                                <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs">EMAIL</h2>
+                                <a className="text-red-500 leading-relaxed">chefrecipe@email.com</a>
+                                <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs mt-4">PHONE</h2>
+                                <p className="leading-relaxed">123-456-7890</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+                        <h2 className="text-gray-900 text-2xl mb-4 font-medium title-font">What you want to Know ?</h2>
+
+                        <div className="relative mb-4">
+                            <label for="name" className="leading-7 text-sm text-gray-600">Name</label>
+                            <input type="text" id="name" name="name" className="w-full bg-white rounded border border-gray-300 focus:border-red-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                        </div>
+                        <div className="relative mb-4">
+                            <label for="email" className="leading-7 text-sm text-gray-600">Email</label>
+                            <input type="email" id="email" name="email" className="w-full bg-white rounded border border-gray-300 focus:border-red-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                        </div>
+                        <div className="relative mb-4">
+                            <label for="message" className="leading-7 text-sm text-gray-600">Message</label>
+                            <textarea id="message" name="message" className="w-full bg-white rounded border border-gray-300 focus:border-red-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+                        </div>
+                        <button className="text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded text-lg">Send Message</button>
+
                     </div>
                 </div>
             </section>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,39 +3,88 @@ import { Link } from 'react-router-dom'
 
 const Footer = () => {
     return (
-        <footer>
-            {/* <div className="bg-gray-100">
-                <div className="w-full md:w-10/12 px-5 py-6 mx-auto flex items-center sm:flex-row flex-col">
-                    <Link to='/' className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-                        <img src="https://i.ibb.co/YP2Nq3Q/Logo-Makr-7u04-SC.png" width={180} alt="" />
-                    </Link>
-                    <p className="text-sm text-gray-500 sm:ml-6 sm:mt-0 mt-4">© 2023 Chef Recipe</p>
-                    <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-                        <a className="text-gray-500">
-                            <svg fill="currentColor" rokeLinecap="round" strokeLinejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
-                                <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                            </svg>
-                        </a>
-                        <a className="ml-3 text-gray-500">
-                            <svg fill="currentColor" rokeLinecap="round" strokeLinejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
-                                <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                            </svg>
-                        </a>
-                        <a className="ml-3 text-gray-500">
-                            <svg fill="none" stroke="currentColor" rokeLinecap="round" strokeLinejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
-                                <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
-                                <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
-                            </svg>
-                        </a>
-                        <a className="ml-3 text-gray-500">
-                            <svg fill="currentColor" stroke="currentColor" rokeLinecap="round" strokeLinejoin="round" stroke-width="0" className="w-5 h-5" viewBox="0 0 24 24">
-                                <path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"></path>
-                                <circle cx="4" cy="4" r="2" stroke="none"></circle>
-                            </svg>
-                        </a>
-                    </span>
+        // <footer>
+        //     <div className="bg-gray-100">
+        //         <div className="w-full md:w-10/12 px-5 py-6 mx-auto flex items-center sm:flex-row flex-col justify-between">
+        //             <Link to='/' className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+        //                 <img src="https://i.ibb.co/YP2Nq3Q/Logo-Makr-7u04-SC.png" width={180} alt="" />
+        //             </Link>
+        //             <p className="text-lg text-gray-500 mt-4 sm:mt-0">&copy; 2023 Chef Recipe | Sanjoy</p>
+
+        //         </div>
+        //     </div>
+        // </footer>
+
+        <footer class="bg-gray-900 text-white mt-8 md:mt-16">
+
+            <div class="w-100 md:w-10/12 pt-20 pb-14 px-3 mx-auto">
+
+                <div class="flex justify-between flex-col md:flex-row pb-12 md:pb-20">
+                    <img src="https://i.ibb.co/YP2Nq3Q/Logo-Makr-7u04-SC.png" class="w-40 md:w-48 h-full mb-5 md:mb-0 cursor-pointer bg-white rounded-md" alt="" />
+                    <div class="flex items-center flex-wrap gap-5 md:gap-10">
+                        <p class="text-xl">Ready to get strated?</p>
+                        <button class="inline-flex text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded">Get Started</button>
+                    </div>
                 </div>
-            </div> */}
+
+
+                <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+                    <div class="mb-5 lg:mb-0">
+                        <h3 class="text-xl font-medium pb-6">Quick Links</h3>
+                        <ul>
+                            <li class="mb-4"><a href="/" class="">Home</a></li>
+                            <li class="mb-4"><a href="/services" class="">Services</a></li>
+                            <li class="mb-4"><a href="/chefs" class="">Chefs</a></li>
+                            <li class="mb-4"><a href="/blogs" class="">Blogs</a></li>
+                        </ul>
+
+                    </div>
+                    <div class="">
+                        <h3 class="text-xl font-medium pb-6">Our Services</h3>
+                        <ul>
+                            <li class="mb-4"><a href="" class="">Real Test</a></li>
+                            <li class="mb-4"><a href="" class="">Best Food</a></li>
+                            <li class="mb-4"><a href="" class="">Best Chefs</a></li>
+                            <li class="mb-4"><a href="" class="">Quick Delivery</a></li>
+
+                        </ul>
+
+                    </div>
+                    <div class="">
+                        <h3 class="text-xl font-medium pb-6">Help</h3>
+                        <ul>
+                            <li class="mb-4"><a href="" class="">FAQs</a></li>
+                            <li class="mb-4"><a href="" class="">Contact Us</a></li>
+                        </ul>
+
+                    </div>
+                    <div class="">
+                        <h3 class="text-xl md:text-3xl  font-medium pb-7">Subscribe to our newsletter</h3>
+
+                        <div class="flex items-center gap-1 mb-10">
+
+                            <div class="relative sm:w-64 w-40 sm:mr-4 mr-2">
+
+                                <input type="text" id="footer-field" name="footer-field" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:ring-2 focus:bg-transparent focus:ring-red-200 focus:border-red-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                            </div>
+                            <button class="inline-flex text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded">Subscribe</button>
+                        </div>
+
+                        <div class="space-x-6">
+                            <i class="fab fa-facebook-f cursor-pointer text-2xl"></i>
+                            <i class="fab fa-twitter cursor-pointer text-2xl"></i>
+                            <i class="fab fa-instagram cursor-pointer text-2xl"></i>
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <div class="text-center text-gray-200 mt-10 md:mt-14">&copy; 2023 - By Sanjoy Sarker - All rights reserved.
+                </div>
+
+            </div>
+
         </footer>
     )
 }
